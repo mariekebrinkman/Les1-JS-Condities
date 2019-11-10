acbead3644f89dade176e29f683701886583c7bd
marieke Brinkman <marieke.brinkman@ziggo.nl>
2019-11-10 15:24:52 +0100

    second

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Les1-JS-Condities
+# Les1-JS-Conditie
+
+Open de bestanden.
+Je hoeft niets aan te passen aan de HTML
+Alleen de JS moet worden aangepast
+schrijf onder elke commentaar regel de pseudo code
+programmeer daarna de JS code
+test je programma
---
+++ script.js
@@ -0,0 +1,24 @@
+function zoekUit() {
+    var aantal = document.getElementById("aantal").value;
+    var duur =  document.getElementById("duur").value;
+    var streaming = document.getElementById("streaming").value;
+
+    var abonnement ="eenvoudig";
+//als je met meer dan 2 personen kijkt en meer dan 3 uur per dag, dan is het beste abonnement familie
+//pseudocode: als (aantal==3 en duur>3 dan abonnement ="familie")
+if ( ) {
+    
+}
+//als je met 2 of meer personen kijkt of al een abonnement hebt, dan is het beste abonnement uitgebreid
+//pseudocode: 
+
+//als je met 2 personen kijkt of meer dan 4 uur per dag dan is het abonnement standaard
+//pseudocode: 
+
+// als je met 1 persoon kijkt en 6 of meer uur per dag dan is het abonnement uitgebreid
+//pseudocode: 
+
+
+//toon het type abonnement op het scherm
+
+}
